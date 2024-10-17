3a2eb4ba5112763e684a87e0f421fd4765c70d49
Ilyasse <developer.elias.essadak@gmail.com>
2024-10-17 14:00:27 +0200

    creando navbar y home

---
+++ src/vista/InicioView.js
@@ -0,0 +1,9 @@
+const InicioView = () => {
+    return (
+        <div className="InicioView">
+            <p>Hola</p>
+        </div>
+    );
+};
+
+export default InicioView;
---
+++ src/vista/NavbarView.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom';
+
+const NavbarView = () => {
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">DameMiDinero</Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to="/">
+                Inicio
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavbarView;
